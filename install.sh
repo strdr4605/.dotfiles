a102f6d31904173dc86ffb8c860af6888fa8e934
Dragoș Străinu <str.dr4605@gmail.com>
2022-02-24 23:29:54 +0200

    Add installer and .gitconfig

---
+++ install.sh
@@ -0,0 +1,11 @@
+BASEDIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+dotfiles=(
+    .zshrc
+    .gitconfig
+)
+for file in "${dotfiles[@]}"; do
+  echo "Force linking ${BASEDIR}/${file} to ~/${file}"
+  ln -sf ${BASEDIR}/${file} ~/${file}
+done
+
