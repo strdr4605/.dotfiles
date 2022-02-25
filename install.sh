f3e726b8b41681f1390ad40d6eeaeb45387bf184
Dragoș Străinu <str.dr4605@gmail.com>
2022-02-25 17:12:25 +0200

    Add message for iTerm2

--- install.sh
+++ install.sh
@@ -1,18 +1,18 @@
 BASEDIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
-echo "Installing Homebrew"
+echo ">>>> Installing Homebrew"
 if test ! $(which brew); then
   /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 fi
 brew update
-echo "Install Homebrew Packages"
+echo ">>>> Install Homebrew Packages"
 brew tap homebrew/bundle
 brew bundle
 
-echo "Installing Oh My Zsh"
+echo ">>>> Installing Oh My Zsh"
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
-echo "Force linking ${BASEDIR}/nvim to ~/.config/nvim"
+echo ">>>> Force linking ${BASEDIR}/nvim to ~/.config/nvim\n\n"
 ln -sf ${BASEDIR}/nvim ~/.config/nvim 
 
 dotfiles=(
@@ -20,7 +20,8 @@ dotfiles=(
     .gitconfig
 )
 for file in "${dotfiles[@]}"; do
-  echo "Force linking ${BASEDIR}/${file} to ~/${file}"
+  echo ">>>> Force linking ${BASEDIR}/${file} to ~/${file}"
   ln -sf ${BASEDIR}/${file} ~/${file}
 done
 
+echo "\n>>>> Please run ./iterm2.sh after opening iTerm2"
