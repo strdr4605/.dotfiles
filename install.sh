58d56748008e221a2f54943e1f32e299b6a5f476
Dragoș Străinu <str.dr4605@gmail.com>
2022-02-25 16:55:10 +0200

    Add nvim with basic options
    Following https://github.com/LunarVim/Neovim-from-scratch

--- install.sh
+++ install.sh
@@ -1,15 +1,5 @@
 BASEDIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
-dotfiles=(
-    .zshrc
-    .gitconfig
-)
-for file in "${dotfiles[@]}"; do
-  echo "Force linking ${BASEDIR}/${file} to ~/${file}"
-  ln -sf ${BASEDIR}/${file} ~/${file}
-done
-
-
 echo "Installing Homebrew"
 if test ! $(which brew); then
   /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
@@ -21,3 +11,16 @@ brew bundle
 
 echo "Installing Oh My Zsh"
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+echo "Force linking ${BASEDIR}/nvim to ~/.config/nvim"
+ln -sf ${BASEDIR}/nvim ~/.config/nvim 
+
+dotfiles=(
+    .zshrc
+    .gitconfig
+)
+for file in "${dotfiles[@]}"; do
+  echo "Force linking ${BASEDIR}/${file} to ~/${file}"
+  ln -sf ${BASEDIR}/${file} ~/${file}
+done
+
