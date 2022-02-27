53c253134873c45cbbf2e2c964e35c48327e6089
Dragoș Străinu <str.dr4605@gmail.com>
2022-02-27 16:51:37 +0200

    Add gitsigns

--- nvim/lua/user/plugins.lua
+++ nvim/lua/user/plugins.lua
@@ -75,6 +75,9 @@ return packer.startup(function(use)
   use "p00f/nvim-ts-rainbow"
   use "JoosepAlviste/nvim-ts-context-commentstring"
 
+  -- Git
+  use "lewis6991/gitsigns.nvim"
+
   -- Automatically set up your configuration after cloning packer.nvim
   -- Put this at the end after all plugins
   if PACKER_BOOTSTRAP then
