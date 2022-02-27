2396a44cd595ec71c69bb22c10943919f3248def
Dragoș Străinu <str.dr4605@gmail.com>
2022-02-27 21:16:04 +0200

    Add Pocco81/AutoSave.nvim

--- nvim/lua/user/plugins.lua
+++ nvim/lua/user/plugins.lua
@@ -45,7 +45,7 @@ return packer.startup(function(use)
   use "nvim-lua/popup.nvim" -- An implementation of the Popup API from vim in Neovim
   use "nvim-lua/plenary.nvim" -- Useful lua functions used ny lots of plugins
   use "numToStr/Comment.nvim" -- Easily comment stuff
-
+  use "Pocco81/AutoSave.nvim"
   use "akinsho/bufferline.nvim"
   use "moll/vim-bbye"
 
