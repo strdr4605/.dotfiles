fb5453c59bf608105ec7eb80c891df69657c86e9
Dragoș Străinu <str.dr4605@gmail.com>
2022-02-27 16:43:09 +0200

    Add comment

--- nvim/lua/user/plugins.lua
+++ nvim/lua/user/plugins.lua
@@ -44,6 +44,7 @@ return packer.startup(function(use)
   use "wbthomason/packer.nvim" -- Have packer manage itself
   use "nvim-lua/popup.nvim" -- An implementation of the Popup API from vim in Neovim
   use "nvim-lua/plenary.nvim" -- Useful lua functions used ny lots of plugins
+  use "numToStr/Comment.nvim" -- Easily comment stuff
 
   -- cmp plugins
   use "hrsh7th/nvim-cmp" -- The completion plugin
@@ -64,7 +65,7 @@ return packer.startup(function(use)
 
   -- Telescope
   use "nvim-telescope/telescope.nvim"
-  use 'nvim-telescope/telescope-media-files.nvim'
+  use "nvim-telescope/telescope-media-files.nvim"
 
   -- Treesitter
   use {
@@ -72,6 +73,7 @@ return packer.startup(function(use)
     run = ":TSUpdate",
   }
   use "p00f/nvim-ts-rainbow"
+  use "JoosepAlviste/nvim-ts-context-commentstring"
 
   -- Automatically set up your configuration after cloning packer.nvim
   -- Put this at the end after all plugins
