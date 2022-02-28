4c2b320ff2265f05ae0c836d045443ef71c6dbc8
Dragoș Străinu <str.dr4605@gmail.com>
2022-02-28 10:14:45 +0200

    Null-ls prettier,eslint prefer node_modules/.bin

--- nvim/lua/user/lsp/null-ls.lua
+++ nvim/lua/user/lsp/null-ls.lua
@@ -7,11 +7,20 @@ end
 local formatting = null_ls.builtins.formatting
 -- https://github.com/jose-elias-alvarez/null-ls.nvim/tree/main/lua/null-ls/builtins/diagnostics
 local diagnostics = null_ls.builtins.diagnostics
+-- https://github.com/jose-elias-alvarez/null-ls.nvim/tree/main/lua/null-ls/builtins/code_actions
+local code_actions = null_ls.builtins.code_actions
 
 null_ls.setup({
 	debug = false,
 	sources = {
-    formatting.prettier,
-    diagnostics.eslint,
+    formatting.prettier.with({
+      prefer_local = "node_modules/.bin",
+    }),
+    diagnostics.eslint.with({
+      prefer_local = "node_modules/.bin",
+    }),
+    code_actions.eslint.with({
+      prefer_local = "node_modules/.bin",
+    }),
 	},
 })
