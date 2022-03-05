781ad8a112c78c843b2f0067593fe83a709e29f0
Dragoș Străinu <str.dr4605@gmail.com>
2022-03-05 23:01:19 +0200

    fix(fugitive): exclude gh-pages branch from Graph command

--- nvim/lua/user/fugitive.lua
+++ nvim/lua/user/fugitive.lua
@@ -1,1 +1,1 @@
-vim.cmd[[ autocmd User Fugitive command! -buffer -bar Graph exe 'terminal' FugitiveShellCommand(['log', '--oneline', '--decorate', '--graph', '--all']) ]]
+vim.cmd[[ autocmd User Fugitive command! -buffer -bar Graph exe 'terminal' FugitiveShellCommand(['log', '--oneline', '--decorate', '--graph', '--exclude=refs/remotes/origin/gh-pages', '--all']) ]]
