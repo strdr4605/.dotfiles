2d375f4df98869068c1bc9cec161654b3b1d84e1
Dragoș Străinu <str.dr4605@gmail.com>
2022-03-12 18:33:22 +0200

    feat(nvim/startify): add custom header, list

--- nvim/lua/user/startify.lua
+++ nvim/lua/user/startify.lua
@@ -1,1 +1,16 @@
 vim.cmd[[ autocmd User StartifyBufferOpened Gcd ]]
+-- https://textkool.com/en/test-ascii-art-generator?text=strdr4605
+vim.g.startify_custom_header = {
+  "        _           _        ___   ____ _____ _____ ",
+  "       | |         | |      /   | / ___|  _  |  ___|",
+  "    ___| |_ _ __ __| |_ __ / /| |/ /___| |/' |___ \\ ",
+  "   / __| __| '__/ _` | '__/ /_| || ___ \\  /| |   \\ \\",
+  "   \\__ \\ |_| | | (_| | |  \\___  || \\_/ \\ |_/ /\\__/ /",
+  "   |___/\\__|_|  \\__,_|_|      |_/\\_____/\\___/\\____/ ",
+}
+vim.g.startify_lists = {
+  { type = 'dir', header = { '   Current Directory ' .. vim.fn.getcwd() } },
+  { type = 'sessions', header = { '   Sessions' } },
+  { type = 'files', header = { '   Most Recently Used' } },
+}
+vim.g.startify_session_persistence = 1
