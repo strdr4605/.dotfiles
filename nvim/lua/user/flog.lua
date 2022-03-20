dacb83d387940d3daddd79d5a9d53e4c6305d2ee
Dragoș Străinu <str.dr4605@gmail.com>
2022-03-20 15:43:28 +0200

    chore(nvim): add author for Flog Graph command

--- nvim/lua/user/flog.lua
+++ nvim/lua/user/flog.lua
@@ -1,2 +1,2 @@
-vim.cmd [[ command! Graph execute 'Flog -format=[%h]\ %d\ %s -- --exclude=refs/remotes/origin/gh-pages --all' ]]
+vim.cmd [[ command! Graph execute 'Flog -format=[%h]\ %d\ {%an}\ %s -- --exclude=refs/remotes/origin/gh-pages --all' ]]
 
