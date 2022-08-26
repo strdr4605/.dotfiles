52299421f849fc9f2e10b007291ec58b72722123
Dragoș Străinu <str.dr4605@gmail.com>
2022-08-26 11:45:20 +0300

    chore(nvim): update to Pocco81/auto-save.nvim

--- nvim/lua/user/plugins.lua
+++ nvim/lua/user/plugins.lua
@@ -45,7 +45,7 @@ return packer.startup(function(use)
   use("nvim-lua/popup.nvim") -- An implementation of the Popup API from vim in Neovim
   use("nvim-lua/plenary.nvim") -- Useful lua functions used ny lots of plugins
   use({ "numToStr/Comment.nvim", config = require("user.comment") })
-  use({ "Pocco81/AutoSave.nvim", config = require("user.autosave") })
+  use({ "Pocco81/auto-save.nvim", config = require("user.auto-save") })
   use("moll/vim-bbye")
   use("tpope/vim-unimpaired")
   use({ "goolord/alpha-nvim", config = require("user.alpha-nvim") })
