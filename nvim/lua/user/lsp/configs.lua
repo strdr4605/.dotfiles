54b255570d05d80931c8650da03fe33f8f469b6e
Dragoș Străinu <str.dr4605@gmail.com>
2022-10-12 11:54:40 +0300

    feat(nvim): add lua-dev.nvim

--- nvim/lua/user/lsp/configs.lua
+++ nvim/lua/user/lsp/configs.lua
@@ -3,6 +3,10 @@ if not status_ok then
   return
 end
 
+require("lua-dev").setup({
+  -- add any options here, or leave empty to use the default settings
+})
+
 local mason = require("mason")
 local lspconfig = require("lspconfig")
 
--- nvim/lua/user/plugins.lua
+++ nvim/lua/user/plugins.lua
@@ -72,6 +72,7 @@ return packer.startup(function(use)
   use({ "kevinhwang91/nvim-bqf" })
   use({ "https://github.com/github/copilot.vim", config = require("user.copilot") })
   use({ "ja-ford/delaytrain.nvim", config = require("user.delaytrain") })
+  use({ "folke/lua-dev.nvim" })
 
   -- Themes
   use("ellisonleao/gruvbox.nvim")
