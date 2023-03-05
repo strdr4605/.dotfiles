ff1550b73fd5d95499b83944df1edbd0773289a0
Dragoș Străinu <str.dr4605@gmail.com>
2023-03-05 19:10:59 +0200

    WIP: copilot.lua

--- nvim/after/plugin/nvim-cmp.lua
+++ nvim/after/plugin/nvim-cmp.lua
@@ -6,6 +6,18 @@ local check_backspace = function()
   return col == 0 or vim.fn.getline("."):sub(col, col):match("%s")
 end
 
+local has_words_before = function()
+  if vim.api.nvim_buf_get_option(0, "buftype") == "prompt" then
+    return false
+  end
+  local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+  return col ~= 0
+      and vim.api
+      .nvim_buf_get_text(0, line - 1, 0, line - 1, col, {})[1]
+      :match("^%s*$")
+      == nil
+end
+
 cmp.setup({
   snippet = {
     expand = function(args)
@@ -13,44 +25,36 @@ cmp.setup({
     end,
   },
   mapping = {
-    ["<C-Space>"] = cmp.mapping(cmp.mapping.complete(), { "i", "c" }),
+    ["<C-Space>"] = cmp.mapping.complete({
+      config = { sources = { { name = "copilot", keyword_length = 0 } } },
+    }),
     ["<C-e>"] = cmp.mapping({
       i = cmp.mapping.abort(),
       c = cmp.mapping.close(),
     }),
     -- Accept currently selected item. If none selected, `select` first item.
     -- Set `select` to `false` to only confirm explicitly selected items.
-    ["<CR>"] = cmp.mapping.confirm({ select = true }),
-    ["<Tab>"] = cmp.mapping(function(fallback)
-      if cmp.visible() then
-        cmp.select_next_item()
-      elseif luasnip.expandable() then
-        luasnip.expand()
-      elseif luasnip.expand_or_jumpable() then
-        luasnip.expand_or_jump()
-      elseif check_backspace() then
-        fallback()
+    ["<CR>"] = cmp.mapping.confirm({
+      behavior = cmp.SelectBehavior.Replace,
+      select = false,
+    }),
+    ["<Tab>"] = vim.schedule_wrap(function(fallback)
+      if cmp.visible() and has_words_before() then
+        cmp.select_next_item({ behavior = cmp.SelectBehavior.Select })
       else
         fallback()
       end
-    end, {
-      "i",
-      "s",
-    }),
-    ["<S-Tab>"] = cmp.mapping(function(fallback)
-      if cmp.visible() then
-        cmp.select_prev_item()
-      elseif luasnip.jumpable(-1) then
-        luasnip.jump(-1)
+    end),
+    ["<S-Tab>"] = vim.schedule_wrap(function(fallback)
+      if cmp.visible() and has_words_before() then
+        cmp.select_prev_item({ behavior = cmp.SelectBehavior.Select })
       else
         fallback()
       end
-    end, {
-      "i",
-      "s",
-    }),
+    end),
   },
   sources = {
+    -- { name = "copilot", keyword_length = 0 },
     { name = "luasnip" },
     { name = "nvim_lsp" },
     { name = "nvim_lua" },
--- nvim/lua/strdr4605/packer.lua
+++ nvim/lua/strdr4605/packer.lua
@@ -97,6 +97,51 @@ return require("packer").startup(function(use)
       "L3MON4D3/LuaSnip", -- snippet engine
     },
   })
+  -- use({
+  --   "zbirenbaum/copilot-cmp",
+  --   after = { "copilot.lua" },
+  --   config = function()
+  --     require("copilot_cmp").setup()
+  --   end,
+  -- })
+  
+  use({
+    "zbirenbaum/copilot.lua",
+    cmd = "Copilot",
+    event = "InsertEnter",
+    config = function()
+      require("copilot").setup({
+        panel = {
+          enabled = true,
+          auto_refresh = false,
+          keymap = {
+            jump_prev = "[[",
+            jump_next = "]]",
+            accept = "<CR>",
+            refresh = "gr",
+            open = "<M-CR>",
+          },
+          layout = {
+            position = "bottom", -- | top | left | right
+            ratio = 0.4,
+          },
+        },
+        suggestion = {
+          enabled = true,
+          auto_trigger = true,
+          debounce = 75,
+          keymap = {
+            accept = "<M-l>",
+            accept_word = false,
+            accept_line = false,
+            next = "<M-]>",
+            prev = "<M-[>",
+            dismiss = "<C-]>",
+          },
+        },
+      })
+    end,
+  })
   use("goolord/alpha-nvim")
   use({
     "nvim-lualine/lualine.nvim",
