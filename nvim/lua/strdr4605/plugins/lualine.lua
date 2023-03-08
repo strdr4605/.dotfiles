55b040b3f6e9b5e83cc951d861ecf96277d02e65
Dragoș Străinu <str.dr4605@gmail.com>
2023-03-08 11:16:07 +0200

    chore(nvim): lualine changes

--- nvim/lua/strdr4605/plugins/lualine.lua
+++ nvim/lua/strdr4605/plugins/lualine.lua
@@ -13,24 +13,16 @@ lualine.setup({
     always_divide_middle = true,
   },
   sections = {
-    lualine_a = { "mode" },
+    lualine_a = {
+      {
+        "mode",
+        fmt = function(str)
+          return str:sub(1, 1)
+        end,
+      },
+    },
     lualine_b = { "branch", "diff", "diagnostics" },
     lualine_c = {
-      {
-        "filename",
-        file_status = true, -- Displays file status (readonly status, modified status)
-        path = 1, -- 0: Just the filename
-        -- 1: Relative path
-        -- 2: Absolute path
-
-        shorting_target = 40, -- Shortens path to leave 40 spaces in the window
-        -- for other components. (terrible name, any suggestions?)
-        symbols = {
-          modified = "[+]", -- Text to show when the file is modified.
-          readonly = "[-]", -- Text to show when the file is non-modifiable or readonly.
-          unnamed = "[No Name]", -- Text to show for unnamed buffers.
-        },
-      },
       {
         git_blame.get_current_blame_text,
         cond = git_blame.is_blame_text_available,
@@ -53,6 +45,32 @@ lualine.setup({
     lualine_y = { "progress" },
     lualine_z = { "location" },
   },
+  winbar = {
+    lualine_a = {},
+    lualine_b = {},
+    lualine_c = {
+      {
+        "filename",
+        path = 1,
+      },
+    },
+    lualine_x = {},
+    lualine_y = {},
+    lualine_z = {},
+  },
+  inactive_winbar = {
+    lualine_a = {},
+    lualine_b = {},
+    lualine_c = {
+      {
+        "filename",
+        path = 1,
+      },
+    },
+    lualine_x = {},
+    lualine_y = {},
+    lualine_z = {},
+  },
   inactive_sections = {
     lualine_a = {},
     lualine_b = {},
@@ -61,18 +79,4 @@ lualine.setup({
     lualine_y = {},
     lualine_z = {},
   },
-  tabline = {
-    lualine_c = {
-      {
-        "filename",
-        path = 1,
-      },
-    },
-    lualine_b = {},
-    lualine_a = {},
-    lualine_x = {},
-    lualine_y = {},
-    lualine_z = {},
-  },
-  extensions = {},
 })
