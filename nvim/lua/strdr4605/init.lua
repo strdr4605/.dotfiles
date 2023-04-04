8a50d863d29fb58678ca2b4a985fc214dd9117c3
Dragoș Străinu <str.dr4605@gmail.com>
2023-04-04 15:24:04 +0300

    chore(nvim): project settings based on .nvimrc.lua

--- nvim/lua/strdr4605/init.lua
+++ nvim/lua/strdr4605/init.lua
@@ -2,3 +2,9 @@ require("strdr4605.remap")
 require("strdr4605.set")
 require("strdr4605.better-quickfix")
 require("strdr4605.lazy")
+
+-- Project specific settings, not sure if this is the best way to do it, maybe will change later
+local local_nvimrc = vim.fn.getcwd()..'/.nvimrc.lua'
+if vim.loop.fs_stat(local_nvimrc) then
+  vim.cmd('source '..local_nvimrc)
+end
