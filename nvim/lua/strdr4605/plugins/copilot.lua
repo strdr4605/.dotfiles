c45cdb7a63f095c7a13c81adb327abd1562ef02e
Dragoș Străinu <str.dr4605@gmail.com>
2023-04-07 20:59:06 +0300

    fix(nvim): fix copilot keymap

--- nvim/lua/strdr4605/plugins/copilot.lua
+++ nvim/lua/strdr4605/plugins/copilot.lua
@@ -19,7 +19,7 @@ require("copilot").setup({
     auto_trigger = true,
     debounce = 75,
     keymap = {
-      accept = "<C-j>",
+      accept = "<C-i>",
       accept_word = false,
       accept_line = "<C-l>",
       next = "<C-n>",
