df269b3c45eec20dd45696ede77db568bcf1be67
Dragoș Străinu <str.dr4605@gmail.com>
2024-02-27 10:09:32 +0200

    feat(nvim): oxc_language_server

--- nvim/init.lua
+++ nvim/init.lua
@@ -602,6 +602,39 @@ require("lazy").setup({
       local lsp_capabilities = require("cmp_nvim_lsp").default_capabilities()
 
       local lspconfig = require("lspconfig")
+      local util = require("lspconfig.util")
+      local configs = require("lspconfig.configs")
+
+      configs.oxc_language_server = {
+        default_config = {
+          cmd = {
+            "oxc_language_server",
+          },
+          filetypes = {
+            "javascript",
+            "javascriptreact",
+            "typescript",
+            "typescriptreact",
+          },
+          root_dir = function(fname)
+            return util.find_package_json_ancestor(fname)
+                or util.find_node_modules_ancestor(fname)
+                or util.find_git_ancestor(fname)
+          end,
+          single_file_support = true,
+          -- settings is corresponding to https://github.com/oxc-project/oxc/blob/de2f83477444ea19e5e370419ab4678652c87d30/editors/vscode/package.json#L61C1-L95
+          settings = {
+            ["enable"] = true,
+            -- ["run"] = "onType",
+          },
+        },
+      }
+
+      lspconfig.oxc_language_server.setup({
+        on_attach = lsp_attach,
+        capabilities = lsp_capabilities,
+      })
+
       require("mason-lspconfig").setup_handlers({
         function(server_name)
           local opts = {
