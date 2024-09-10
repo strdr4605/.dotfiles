c76d0975cf34c05bee4d25989de78c38790a0ffd
Dragoș Străinu <str.dr4605@gmail.com>
2024-09-10 15:34:21 +0300

    chore(nvim): change nvim-treesitter/nvim-treesitter-context settings

--- nvim/init.lua
+++ nvim/init.lua
@@ -348,6 +348,19 @@ require("lazy").setup({
           additional_vim_regex_highlighting = false,
         },
       })
+
+      require("treesitter-context").setup({
+        enable = true,
+        max_lines = 5, -- Changed this from default config
+        min_window_height = 0,
+        line_numbers = true,
+        multiline_threshold = 1, -- Changed this from default config
+        trim_scope = "outer",
+        mode = "cursor",
+        separator = nil,
+        zindex = 20,
+        on_attach = nil,
+      })
     end,
     dependencies = {
       "windwp/nvim-ts-autotag",
