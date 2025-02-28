2c3ffc9702708a8eeefcac79905238bc356862f3
Dragoș Străinu <str.dr4605@gmail.com>
2025-02-28 11:59:37 +0200

    feat(nvim): replace nvim-cmp with blink.cmp, update plugins

--- nvim/init.lua
+++ nvim/init.lua
@@ -514,15 +514,18 @@ require("lazy").setup({
   },
   {
     "soulsam480/nvim-oxlint",
-    opts = function()
-      local lsp_capabilities = require("cmp_nvim_lsp").default_capabilities()
-      return {
-        capabilities = lsp_capabilities,
-      }
-    end,
+    opts = {},
   }, -- lsp
   {
     "neovim/nvim-lspconfig",
+    dependencies = {
+      "saghen/blink.cmp",
+      "williamboman/mason-lspconfig.nvim",
+      "williamboman/mason.nvim",
+      "yioneko/nvim-vtsls",
+      "nvimtools/none-ls.nvim",
+      "lukas-reineke/lsp-format.nvim",
+    },
     config = function()
       local setup = function()
         local config = {
@@ -648,7 +651,7 @@ require("lazy").setup({
           formatting.stylua,
         },
       })
-      local lsp_capabilities = require("cmp_nvim_lsp").default_capabilities()
+      local lsp_capabilities = require("blink.cmp").get_lsp_capabilities()
 
       local lspconfig = require("lspconfig")
       local util = require("lspconfig.util")
@@ -750,94 +753,88 @@ require("lazy").setup({
         end,
       })
     end,
-    dependencies = {
-      "williamboman/mason-lspconfig.nvim",
-      "williamboman/mason.nvim",
-      "yioneko/nvim-vtsls",
-      "nvimtools/none-ls.nvim",
-      "lukas-reineke/lsp-format.nvim",
-    },
   },
   -- cmp
   {
-    "hrsh7th/nvim-cmp",
-    config = function()
-      local cmp = require("cmp")
+    "saghen/blink.cmp",
+    version = "*",
 
-      local has_words_before = function()
-        if vim.api.nvim_buf_get_option(0, "buftype") == "prompt" then
-          return false
-        end
-        local line, col = unpack(vim.api.nvim_win_get_cursor(0))
-        return col ~= 0
-            and vim.api.nvim_buf_get_text(0, line - 1, 0, line - 1, col, {})[1]:match("^%s*$") == nil
-      end
+    ---@module 'blink.cmp'
+    ---@type blink.cmp.Config
+    opts = {
+      -- 'default' (recommended) for mappings similar to built-in completions (C-y to accept, C-n/C-p for up/down)
+      -- 'super-tab' for mappings similar to vscode (tab to accept, arrow keys for up/down)
+      -- 'enter' for mappings similar to 'super-tab' but with 'enter' to accept
+      --
+      -- All presets have the following mappings:
+      -- C-space: Open menu or open docs if already open
+      -- C-e: Hide menu
+      -- C-k: Toggle signature help
+      --
+      -- See the full "keymap" documentation for information on defining your own keymap.
+      keymap = {
+        preset = "default",
+        ["<CR>"] = { "accept", "fallback" },
+        ["<ESC>"] = { "cancel", "fallback" },
+      },
 
-      cmp.setup({
-        mapping = {
-          ["<C-Space>"] = cmp.mapping.complete({
-            config = { sources = { { name = "copilot", keyword_length = 0 } } },
-          }),
-          ["<C-e>"] = cmp.mapping({
-            i = cmp.mapping.abort(),
-            c = cmp.mapping.close(),
-          }),
-          -- Accept currently selected item. If none selected, `select` first item.
-          -- Set `select` to `false` to only confirm explicitly selected items.
-          ["<CR>"] = cmp.mapping.confirm({
-            behavior = cmp.SelectBehavior.Replace,
-            select = true,
-          }),
-          ["<Tab>"] = vim.schedule_wrap(function(fallback)
-            if cmp.visible() and has_words_before() then
-              cmp.select_next_item({ behavior = cmp.SelectBehavior.Select })
-            else
-              fallback()
-            end
-          end),
-          ["<S-Tab>"] = vim.schedule_wrap(function(fallback)
-            if cmp.visible() and has_words_before() then
-              cmp.select_prev_item({ behavior = cmp.SelectBehavior.Select })
-            else
-              fallback()
-            end
-          end),
+      appearance = {
+        -- Sets the fallback highlight groups to nvim-cmp's highlight groups
+        -- Useful for when your theme doesn't support blink.cmp
+        -- Will be removed in a future release
+        use_nvim_cmp_as_default = true,
+        -- Set to 'mono' for 'Nerd Font Mono' or 'normal' for 'Nerd Font'
+        -- Adjusts spacing to ensure icons are aligned
+        nerd_font_variant = "normal",
+        kind_icons = {
+          Text = "txt",
+          Method = "method",
+          Function = "fn",
+          Constructor = "new",
+          Field = "field",
+          Variable = "var",
+          Property = "prop",
+          Class = "class",
+          Interface = "interface",
+          Struct = "struct",
+          Module = "module",
+          Unit = "unit",
+          Value = "val",
+          Enum = "enum",
+          EnumMember = "enum-member",
+          Keyword = "keyword",
+          Constant = "const",
+          Snippet = "snippet",
+          Color = "color",
+          File = "file",
+          Reference = "ref",
+          Folder = "dir",
+          Event = "event",
+          Operator = "operator",
+          TypeParameter = "type",
         },
-        sources = {
-          -- { name = "copilot", keyword_length = 0 },
-          { name = "luasnip" },
-          { name = "nvim_lsp" },
-          { name = "nvim_lua" },
-          { name = "buffer" },
-          { name = "path" },
-        },
-      })
+      },
 
-      -- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
-      -- `/` cmdline setup.
-      cmp.setup.cmdline("/", {
-        mapping = cmp.mapping.preset.cmdline(),
-        sources = {
-          { name = "buffer" },
+      -- Default list of enabled providers defined so that you can extend it
+      -- elsewhere in your config, without redefining it, due to `opts_extend`
+      sources = {
+        default = { "lsp", "path", "snippets", "buffer" },
+      },
+
+      cmdline = {
+        completion = {
+          menu = { auto_show = true },
         },
-      })
-      -- `:` cmdline setup.
-      cmp.setup.cmdline(":", {
-        mapping = cmp.mapping.preset.cmdline(),
-        sources = cmp.config.sources({
-          { name = "path" },
-        }, {
-          { name = "cmdline" },
-        }),
-      })
-    end,
-    dependencies = {
-      "hrsh7th/cmp-buffer", -- buffer completions
-      "hrsh7th/cmp-path", -- path completions
-      "hrsh7th/cmp-cmdline", -- cmdline completions
-      "hrsh7th/cmp-nvim-lsp",
-      "hrsh7th/cmp-nvim-lua",
+      },
+
+      -- Blink.cmp uses a Rust fuzzy matcher by default for typo resistance and significantly better performance
+      -- You may use a lua implementation instead by using `implementation = "lua"` or fallback to the lua implementation,
+      -- when the Rust fuzzy matcher is not available, by using `implementation = "prefer_rust"`
+      --
+      -- See the fuzzy documentation for more information
+      fuzzy = { implementation = "prefer_rust_with_warning" },
     },
+    opts_extend = { "sources.default" },
   },
   -- {
   --   "supermaven-inc/supermaven-nvim",
