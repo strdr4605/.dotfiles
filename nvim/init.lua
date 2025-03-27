dbb659a5b98c2cc2ec724485d9683f4ba9fa7749
Dragoș Străinu <str.dr4605@gmail.com>
2025-03-27 10:33:57 +0200

    chore(nvim-0.11): use rounded borders on all floating windows

--- nvim/init.lua
+++ nvim/init.lua
@@ -112,6 +112,8 @@ vim.opt.iskeyword:append("-")                                                   
 vim.opt.guicursor =
 "n-v-c:block,i-ci-ve:ver25,r-cr:hor20,o:hor50,a:blinkwait700-blinkoff400-blinkon250-Cursor/lCursor,sm:block-blinkwait175-blinkoff150-blinkon175"  -- setting for guicursor taken from :h 'guicursor'
 
+vim.opt.winborder = "rounded"
+
 vim.api.nvim_create_augroup("JavascriptMacros", { clear = true })
 vim.api.nvim_create_autocmd("FileType", {
   pattern = { "javascript", "javascriptreact", "typescript", "typescriptreact" },
