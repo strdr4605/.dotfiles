700a6217c1eaaf7fafa7e01e5834052c03614564
Dragoș Străinu <str.dr4605@gmail.com>
2025-03-27 10:55:16 +0200

    chore(neovim-0.11): remove redundant keymaps, use default
    https://gpanders.com/blog/whats-new-in-neovim-0-11/#more-default-mappings

--- nvim/init.lua
+++ nvim/init.lua
@@ -440,7 +440,6 @@ require("lazy").setup({
   },
   "tpope/vim-dispatch",
   "tpope/vim-fugitive",
-  "tpope/vim-unimpaired",
   "tpope/vim-surround",
   {
     "lewis6991/gitsigns.nvim",
@@ -618,9 +617,6 @@ require("lazy").setup({
         vim.keymap.set("n", "gd", function()
           vim.lsp.buf.definition()
         end, opts)
-        vim.keymap.set("n", "gr", function()
-          vim.lsp.buf.references()
-        end, opts)
         vim.keymap.set("n", "gl", function()
           vim.diagnostic.open_float()
         end, opts)
@@ -630,26 +626,11 @@ require("lazy").setup({
         vim.keymap.set("n", "<C-k>", function()
           vim.lsp.buf.signature_help()
         end, opts)
-        vim.keymap.set("n", "<leader>rn", function()
-          vim.lsp.buf.rename()
-        end, opts)
-        vim.keymap.set("n", "<leader>ca", function()
-          vim.lsp.buf.code_action()
-        end, opts)
-        vim.keymap.set("n", "[d", function()
-          vim.diagnostic.goto_prev({ border = "rounded" })
-        end, opts)
-        vim.keymap.set("n", "]d", function()
-          vim.diagnostic.goto_next({ border = "rounded" })
-        end, opts)
         vim.keymap.set("n", "[e", function()
           vim.diagnostic.goto_prev({ border = "rounded", severity = vim.diagnostic.severity.ERROR })
         end, opts)
         vim.keymap.set("n", "]e", function()
           vim.diagnostic.goto_next({ border = "rounded", severity = vim.diagnostic.severity.ERROR })
-        end, opts)
-        vim.keymap.set("n", "<leader>q", function()
-          vim.diagnostic.setloclist()
         end, opts)
 
         vim.api.nvim_create_user_command("F", function()
