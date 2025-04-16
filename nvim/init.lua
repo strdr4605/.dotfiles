6efc0e09bc645959bf314e85fcdad3ed3adf933a
Dragoș Străinu <str.dr4605@gmail.com>
2025-04-16 08:35:11 +0300

    fix(nvim): blink.cmp disable auto_brackets

--- nvim/init.lua
+++ nvim/init.lua
@@ -807,6 +807,7 @@ require("lazy").setup({
       completion = {
         documentation = { auto_show = true, auto_show_delay_ms = 500 },
         list = { selection = { preselect = false, auto_insert = false } },
+        accept = { auto_brackets = { enabled = false } },
       },
       sources = {
         default = { "lsp", "path", "buffer" },
